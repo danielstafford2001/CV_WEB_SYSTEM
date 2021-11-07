15a79cee7a0143e0eb5eaa2a4bbc8aab26b3c922
Daniel Stafford <ZHAC200@live.rhul.ac.uk>
2021-11-07 16:39:22 +0000

    Trying to get Forgot Password email to work

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -17,8 +17,8 @@ login_manager.login_message_category = 'info'
 app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
-app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+app.config['MAIL_USERNAME'] = 'danielstafford995'
+app.config['MAIL_PASSWORD'] = 'Daniel2021!'
 mail = Mail(app)
 
 from flaskblog import routes
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -160,10 +160,11 @@ def user_posts(username):
 def send_reset_email(user):
     token = user.get_reset_token()
     msg = Message('Password Reset Request',
-                  sender='daniel.stafford@me.com',
+                  sender='danielstafford995@gmail.com',
                   recipients=[user.email])
     msg.body = f'''To reset your password, visit the following link:
 {url_for('reset_token', token=token, _external=True)}
+
 If you did not make this request then simply ignore this email and no changes will be made.
 '''
     mail.send(msg)
